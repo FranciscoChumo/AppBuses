b0407f861e355f6b0e9ef13d40f527beea0829d5
FranciscoChumo <frchd.1316334083@gmail.com>
2024-12-02 19:49:48 -0500

    CSS

--- src/app/admin/admin.page.ts
+++ src/app/admin/admin.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { IonHeader, IonToolbar, IonContent, IonTabBar, 
   IonTabButton, IonIcon, IonLabel, IonCard, IonButton, IonFab, IonFabButton, IonInput, IonItem, IonLoading } from '@ionic/angular/standalone';
-import { cog, search, person ,mail,create,trash,add, home,close} from 'ionicons/icons';
+import { cog, search, person ,mail,create,trash,add, home,close, exit} from 'ionicons/icons';
 import { addIcons } from 'ionicons';
 import{ chevronDownCircle,
   chevronForwardCircle,
@@ -23,7 +23,7 @@ import{ chevronDownCircle,
 export class AdminPage implements OnInit {
 users:any;
   constructor() {
-    addIcons({ cog, search ,person, mail,create,trash,add,home,close});
+    addIcons({ cog, search ,person, mail,create,trash,add,home,close,exit});
     addIcons({ chevronDownCircle, chevronForwardCircle, chevronUpCircle, colorPalette, document, globe });
 
    }
